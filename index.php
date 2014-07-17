90609d9bcbe4b4b3dbca91acdee6d0d49419f940
Felipe Neuhauss <felipeneuhauss@MacBook-Pro-de-Felipe.local>
2014-07-17 16:44:15 -0300

    Modify readme

--- index.php
+++ index.php
@@ -11,4 +11,35 @@ require_once 'KinveyREST.php';
  * Retrieve
  */
 
+use KinveyREST\KinveyREST;
 
+$kinveyRest = New
+
+$users = $kinveyRest->users(null, null, array('fields' => array('name', 'email', '_id')));
+/**
+ * Obtem as vendas dos usuarios
+ */
+foreach ($users as &$user) {
+    $user->sales = $kinveyRest->retrieve('Sales',
+        '',
+        array('_acl.creator' => $user->_id),
+        array('sort' => array('_kmd.lmt' => -1),
+            'limit' => 1),
+        array(''));
+
+    $responses = array();
+    if (count($user->sales)) {
+        foreach ($user->sales as $sale) {
+            # verifica se fez venda a mais de 15 dias
+            $lastSaleDate = new \DateTime($sale->_kmd->lmt);
+            $today        = new \DateTime('now');
+            $diff         = $today->diff($lastSaleDate);
+            if ($diff->days >= 15) {
+                # Manda e-mail
+                $renderer = $this->getServiceLocator()->get('ViewRenderer');
+                $content = $renderer->partial('application/notification/notsale-email',  array('user' => $user));
+                $responses[] = $mandrill->send($user->email , $user->name,  'Izie - Há 15 dias que você não vende', $content);
+            }
+        }
+    }
+}
--- kinveyREST.php
+++ kinveyREST.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-namespace KinveyRest;
+namespace KinveyREST;
 
 class KinveyREST
 {
